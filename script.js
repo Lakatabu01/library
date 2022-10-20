3251b68d9d6c4bdb8d2801587c0a8d66a615633e
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2022-10-20 11:46:07 -0400

    validate using javascript

--- script.js
+++ script.js
@@ -1,196 +1,228 @@
 (function module() {
-let newBook
-let numOfBooks 
+  let newBook;
+  let numOfBooks;
 
-const sample = document.querySelector(".sample")
-const sample1 = document.querySelector(".sample1")
-const sample2 = document.querySelector(".sample2")
-const radios = document.getElementsByName("status")
-const body = document.querySelector(".body")
-const card = document.querySelector(".card")
-const popUp =  document.querySelector(".addition")
-let para = document.querySelector(".total")
+  const sample = document.querySelector(".sample");
+  const sample1 = document.querySelector(".sample1");
+  const sample2 = document.querySelector(".sample2");
+  const radios = document.getElementsByName("status");
+  const body = document.querySelector(".body");
+  const card = document.querySelector(".card");
+  const popUp = document.querySelector(".addition");
+  let para = document.querySelector(".total");
+  const form = document.querySelector("form");
 
-const addButton = document.querySelector(".add-button")
-addButton.addEventListener("click", openForm)
-const cancel = document.querySelector(".cancel")
-cancel.addEventListener("click", closeForm)
-const submit = document.querySelector(".submit")
-submit.addEventListener("click", addBookToLibrary)
+  const addButton = document.querySelector(".add-button");
+  addButton.addEventListener("click", openForm);
+  const cancel = document.querySelector(".cancel");
+  cancel.addEventListener("click", closeForm);
+  const submit = document.querySelector(".submit");
+  submit.addEventListener("click", addBookToLibrary);
 
-class Book{
- constructor(title, author, pages){
-  this.title = title
-  this.author = author
-  this.pages = pages
-}
- get info(){
-  return  this.title + " by " + this.author + ", " + this.pages + ", " + this.status 
-}
+  sample.addEventListener("input", (event) => {
+    if (sample.validity.tooShort) {
+      sample.setCustomValidity("Length must be between 2 and 20 characters");
+      sample.reportValidity();
+    } else {
+      sample.setCustomValidity("");
+    }
+  });
 
-}
+  sample1.addEventListener("input", (event) => {
+    if (sample1.validity.tooShort) {
+      sample1.setCustomValidity("Length must be between 2 and 20 characters");
+      sample1.reportValidity();
+    } else {
+      sample1.setCustomValidity("");
+    }
+  });
 
-//Code for manually added book
-demo = new Book("Unmasked", "Frank Stone", "223")
-let temporaryLibrary = [demo,]
-let myLibrary = [demo,]
-demoLoop()
+  sample2.addEventListener("input", (event) => {
+    if (sample2.validity.tooShort) {
+      sample2.setCustomValidity("Must be more two or more pages");
+      sample2.reportValidity();
+    } else {
+      sample2.setCustomValidity("");
+    }
+  });
 
+  class Book {
+    constructor(title, author, pages) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+    }
+    get info() {
+      return (
+        this.title +
+        " by " +
+        this.author +
+        ", " +
+        this.pages +
+        ", " +
+        this.status
+      );
+    }
+  }
 
-function addBookToLibrary(){
-if (sample.value.length > 20 || sample.value.length < 1 
-    || sample1.value.length > 20 || sample1.value.length < 1
-    || sample2.value.length < 1){
-  return
-}
- newBook = new Book(sample.value, sample1.value, sample2.value)
+  //Code for manually added book
+  demo = new Book("Unmasked", "Frank Stone", "223");
+  let temporaryLibrary = [demo];
+  let myLibrary = [demo];
+  demoLoop();
 
+  function addBookToLibrary() {
+    if (
+      sample.value.length > 20 ||
+      sample.value.length < 2 ||
+      sample1.value.length > 20 ||
+      sample1.value.length < 2 ||
+      sample2.value.length < 1
+    ) {
+      return;
+    }
 
-myLibrary.push(newBook)
-temporaryLibrary = []
-temporaryLibrary.push(newBook)
-myLibraryLoop()
+    newBook = new Book(sample.value, sample1.value, sample2.value);
 
+    myLibrary.push(newBook);
+    temporaryLibrary = [];
+    temporaryLibrary.push(newBook);
+    myLibraryLoop();
 
- numOfBooks = myLibrary.length 
- para.textContent = "Total number of books: " + myLibrary.length
- popUp.style.display = "none"
- alert("Book added successfully")
-}
+    numOfBooks = myLibrary.length;
+    para.textContent = "Total number of books: " + myLibrary.length;
+    popUp.style.display = "none";
+    alert("Book added successfully");
+  }
 
+  function myLibraryLoop() {
+    for (copy of temporaryLibrary) {
+      let div = document.createElement("div");
+      let title = document.createElement("p");
+      let author = document.createElement("p");
+      let pages = document.createElement("p");
+      let statusOfBook = document.createElement("p");
+      let image = document.createElement("img");
+      let wrapper = document.createElement("div");
+      let imageDiv = document.createElement("div");
+      let checkbox = document.querySelector(".checkbox");
 
-function myLibraryLoop(){
-    for (copy of temporaryLibrary){
-        let div = document.createElement("div")
-        let title = document.createElement("p")
-         let author = document.createElement("p")
-         let pages = document.createElement("p")
-         let statusOfBook = document.createElement("p")
-         let image = document.createElement("img")
-         let wrapper = document.createElement("div")
-         let imageDiv = document.createElement("div")
-         let checkbox = document.querySelector(".checkbox")
+      let seenDiv = document.createElement("div");
+      let seenInput = document.createElement("input");
+      seenInput.type = "checkbox";
+      let label = document.createElement("label");
+      label.textContent = "Read";
+      label.style.paddingLeft = "4px";
 
-         let seenDiv = document.createElement("div")
-         let seenInput = document.createElement("input")
-         seenInput.type = "checkbox"
-         let label = document.createElement("label")
-         label.textContent = "Read"
-         label.style.paddingLeft = "4px"
-         
+      remove = document.createElement("button");
 
-         remove = document.createElement("button")
-         
-         removedBooks = myLibrary.length - 1
-         remove.addEventListener("click", test)
-         image.src = "delete-off.png"
-          
-         title.classList.add("child")
-  author.classList.add("child")
-  pages.classList.add("child")
-  statusOfBook.classList.add("child")
-  seenDiv.classList.add("checkbox")
+      removedBooks = myLibrary.length - 1;
+      remove.addEventListener("click", test);
+      image.src = "delete-off.png";
 
-  title.textContent = "Title: " + newBook.title
-  author.textContent = "Author: " + newBook.author
-  pages.textContent = "Pages: " + newBook.pages
-  
+      title.classList.add("child");
+      author.classList.add("child");
+      pages.classList.add("child");
+      statusOfBook.classList.add("child");
+      seenDiv.classList.add("checkbox");
 
-body.appendChild(wrapper)
-wrapper.appendChild(imageDiv)
-wrapper.appendChild(div)
-imageDiv.appendChild(remove)
+      title.textContent = "Title: " + newBook.title;
+      author.textContent = "Author: " + newBook.author;
+      pages.textContent = "Pages: " + newBook.pages;
 
-seenDiv.appendChild(seenInput)
-seenDiv.appendChild(label)
-remove.appendChild(image)
+      body.appendChild(wrapper);
+      wrapper.appendChild(imageDiv);
+      wrapper.appendChild(div);
+      imageDiv.appendChild(remove);
 
-div.appendChild(title)
-div.appendChild(author)
-div.appendChild(pages)
-div.appendChild(seenDiv)
+      seenDiv.appendChild(seenInput);
+      seenDiv.appendChild(label);
+      remove.appendChild(image);
 
+      div.appendChild(title);
+      div.appendChild(author);
+      div.appendChild(pages);
+      div.appendChild(seenDiv);
 
-//function for delete button
-function test (){
-  body.removeChild(wrapper)
-  myLibrary.pop()
-  para.textContent = "Total number of books: " + myLibrary.length
-}
+      //function for delete button
+      function test() {
+        body.removeChild(wrapper);
+        myLibrary.pop();
+        para.textContent = "Total number of books: " + myLibrary.length;
+      }
 
-wrapper.classList.add("card")
-imageDiv.classList.add("delete")
-remove.classList.add("btn")
-    } 
-}
+      wrapper.classList.add("card");
+      imageDiv.classList.add("delete");
+      remove.classList.add("btn");
+    }
+  }
 
- function openForm(){
-  sample.value = ""
-  sample1.value = ""
-  sample2.value = ""
-  popUp.style.display = "block"
- }
+  function openForm() {
+    sample.value = "";
+    sample1.value = "";
+    sample2.value = "";
+    popUp.style.display = "block";
+  }
 
- function closeForm(){
-   popUp.style.display = "none"
- }
- 
- function demoLoop(){
-  for (copy of temporaryLibrary){
-      let div = document.createElement("div")
-      let title = document.createElement("p")
-       let author = document.createElement("p")
-       let pages = document.createElement("p")
-       let statusOfBook = document.createElement("p")
-       let image = document.createElement("img")
-       let wrapper = document.createElement("div")
-       let imageDiv = document.createElement("div")
-       remove = document.createElement("button")
-       
-        let checkbox = document.querySelector(".checkbox")
-        let seenDiv = document.createElement("div")
-         let seenInput = document.createElement("input")
-         seenInput.type = "checkbox"
-         let label = document.createElement("label")
-         label.textContent = "Read"
-         label.style.paddingLeft = "4px"
-       removedBooks = myLibrary.length - 1
-       remove.addEventListener("click", () => {
-        body.removeChild(wrapper)
-        myLibrary.pop()
-        para.textContent = "Total number of books: " + myLibrary.length
-        myLibrary = []
-       })
-       image.src = "delete-off.png"
-        
-       title.classList.add("child")
-author.classList.add("child")
-pages.classList.add("child")
-statusOfBook.classList.add("child")
-seenDiv.classList.add("checkbox")
+  function closeForm() {
+    popUp.style.display = "none";
+  }
 
-title.textContent = "Title: " + demo.title
-author.textContent = "Author: " + demo.author
-pages.textContent = "Pages: " + demo.pages
-statusOfBook.textContent = "Status: " + demo.statusOfBook
+  function demoLoop() {
+    for (copy of temporaryLibrary) {
+      let div = document.createElement("div");
+      let title = document.createElement("p");
+      let author = document.createElement("p");
+      let pages = document.createElement("p");
+      let statusOfBook = document.createElement("p");
+      let image = document.createElement("img");
+      let wrapper = document.createElement("div");
+      let imageDiv = document.createElement("div");
+      remove = document.createElement("button");
 
-body.appendChild(wrapper)
-wrapper.appendChild(imageDiv)
-wrapper.appendChild(div)
-imageDiv.appendChild(remove)
+      let checkbox = document.querySelector(".checkbox");
+      let seenDiv = document.createElement("div");
+      let seenInput = document.createElement("input");
+      seenInput.type = "checkbox";
+      let label = document.createElement("label");
+      label.textContent = "Read";
+      label.style.paddingLeft = "4px";
+      removedBooks = myLibrary.length - 1;
+      remove.addEventListener("click", () => {
+        body.removeChild(wrapper);
+        myLibrary.pop();
+        para.textContent = "Total number of books: " + myLibrary.length;
+        myLibrary = [];
+      });
+      image.src = "delete-off.png";
 
-seenDiv.appendChild(seenInput)
-seenDiv.appendChild(label)
-remove.appendChild(image)
-div.appendChild(title)
-div.appendChild(author)
-div.appendChild(pages)
-div.appendChild(seenDiv)
+      title.classList.add("child");
+      author.classList.add("child");
+      pages.classList.add("child");
+      statusOfBook.classList.add("child");
+      seenDiv.classList.add("checkbox");
 
+      title.textContent = "Title: " + demo.title;
+      author.textContent = "Author: " + demo.author;
+      pages.textContent = "Pages: " + demo.pages;
+      statusOfBook.textContent = "Status: " + demo.statusOfBook;
 
-wrapper.classList.add("card")
-imageDiv.classList.add("delete")
-remove.classList.add("btn")
-  } 
-}
-})()
+      body.appendChild(wrapper);
+      wrapper.appendChild(imageDiv);
+      wrapper.appendChild(div);
+      imageDiv.appendChild(remove);
+
+      seenDiv.appendChild(seenInput);
+      seenDiv.appendChild(label);
+      remove.appendChild(image);
+      div.appendChild(title);
+      div.appendChild(author);
+      div.appendChild(pages);
+      div.appendChild(seenDiv);
+
+      wrapper.classList.add("card");
+      imageDiv.classList.add("delete");
+      remove.classList.add("btn");
+    }
+  }
+})();
